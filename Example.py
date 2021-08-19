20bd71ec7b15302d004a8417f35a1ce2f6e457cf
AaronVuong <41023512+AaronVuong@users.noreply.github.com>
2021-08-19 04:05:02 -0400

    Able to input date, create tables and put data into csv

--- Example.py
+++ Example.py
@@ -1,15 +1,23 @@
-import requests, sys, csv
-
+import requests, sys, csv, json
+from prettytable import from_csv
 
 #MSW, Mixed Sweet Blend, crudes
 acronym = "none"
 name = "none"
 database = "none"
 URL = "https://www.crudemonitor.ca/savePHPExcel.php"
-
+start_date = "none"
+end_date = "none"
 
 if("--acronym" in sys.argv):
     acronym = sys.argv[sys.argv.index("--acronym")+1]
+    
+if("--start_date" in sys.argv):
+    start_date = sys.argv[sys.argv.index("--start_date")+1]
+    
+if("--end_date" in sys.argv):
+    end_date = sys.argv[sys.argv.index("--end_date")+1]
+    
 
 
 #reading CSV file to find the other 2 parameters 
@@ -22,7 +30,8 @@ with open('Crude Monitor Parameters.csv', newline = '') as csvfile:
             database = row['database']
             break
                         
-    
+ 
+# Date should be in DDMMYYYY form
 form_data = {
     "date1noscript": "",
     "date2noscript": "",
@@ -32,14 +41,24 @@ form_data = {
     "db": database,
     "basicanalysis[]": "AbsoluteDensity",
     "options": "on",
-    "date1": "13072019",
-    "date2": "13072021",
+    "date1": start_date,
+    "date2": end_date,
     "format": "Export .JSON",
     "daterangepicker_start": "",
     "daterangepicker_end": "",
 }
 
 data = requests.post(url=URL, data=form_data)
-print(data.text)
-print(reader)
+#data.json()
+#with open(data.text) as f:
+    #data_pulled = [json.loads(line) for line in f]
+
+lines = data.text.splitlines()
+
+reader = csv.reader(lines)
+parsed_csv = list(reader)
+with open(parsed_csv) as fp:
+    mytable = from_csv(fp)
+
+print(mytable)
 
